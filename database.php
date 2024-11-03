1af09d1280ca892a9e03673b4c8ec61fa7453481
Egamberdiyev-Akmal <egamberdiyevakmal2000@gmail.com>
2024-11-03 20:37:39 +0500

    database.php file create

--- database.php
+++ database.php
@@ -8,6 +8,6 @@ try {
     $coon = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
     $coon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
-} catch (PDOException $err) {
+} catch (PDOException $err) {     
     echo "Connected failed : " . $err->getMessage();
 }
