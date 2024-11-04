4bacf7732ca82a010aaea6b0dd3490c765c23fd3
Egamberdiyev-Akmal <egamberdiyevakmal2000@gmail.com>
2024-11-04 19:07:40 +0500

    id orqali malumot olindi

--- post.php
+++ post.php
@@ -2,10 +2,20 @@
 $title = "Postkni ko'rish";
 require './includes/header.php';
 require './database.php';
+$id = $_GET['id'];
 
-$statement=$pdo->prepare("");
+$statement = $pdo->prepare("SELECT * FROM posts WHERE id=?");
+$statement->execute([$id]);
+$post = $statement->fetch();
+var_dump($post);
+echo 'salom'
 ?>
 
-
+<div class="container mt-5">
+    <h2>Id: <?= $post['id'] ?></h2>
+    <h1>Sarlavha: <?= $post['sarlavhaa'] ?></h1>
+    <p> Matin: <?= $post['matinn'] ?></p>
+    <p> Yarailgan vaqti: <?= $post['create_at'] ?></p>
+</div>
 
 <?php require './includes/footer.php' ?>
